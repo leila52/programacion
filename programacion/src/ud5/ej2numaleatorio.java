30e567bf175559324e1e8af6980f9cd46a30ff3a
leila <leila@1DAW-19>
2023-11-30 14:16:05 +0100

    14:16

--- programacion/src/ud5/ej2numaleatorio.java
+++ programacion/src/ud5/ej2numaleatorio.java
@@ -5,13 +5,15 @@ import java.util.Random;
 public class ej2numaleatorio {
 	public static void main(String[] args) {
 		int suma=0;
-		for(int i=0;i<3;i++) {
-			Random ale = null;
-			ale=new Random();
-			int tirada=ale.nextInt(6)+1;
-			System.out.println(tirada);
-			suma=suma+tirada;
-	}
+		Random ale = null;
+		ale=new Random();
+		int tirada=ale.nextInt(6)+1;
+		int tirada2=ale.nextInt(6)+1;
+		int tirada3=ale.nextInt(6)+1;
+		System.out.println(tirada);
+		System.out.println(tirada2);
+		System.out.println(tirada3);
+		suma=tirada+tirada2+tirada3;
 		System.out.println("la suma de los tres dados es"+suma);
 	}
 
