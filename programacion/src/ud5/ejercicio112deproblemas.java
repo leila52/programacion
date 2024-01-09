1b5db4fa6eb19af34dd9cf070ac12225462da766
leila_16 <leila_16@leila.home>
2024-01-09 19:36:56 +0100

    19:36

--- programacion/src/ud5/ejercicio112deproblemas.java
+++ programacion/src/ud5/ejercicio112deproblemas.java
@@ -7,12 +7,38 @@ public class ejercicio112deproblemas {
 	public static void main(String[] args) {
 		String cad;
 		String cad2;
-		do {
 			System.out.println("introduce la primera de cadena");
-			cad= entrada.nextLine().
-		}while((cad.length()>0 && cad.length()<10 )&& (cad.length()==cad2.length());
+			cad= entrada.nextLine().toLowerCase();
+			System.out.println("ingrese la segunda cadena");
+			cad2=entrada.nextLine().toLowerCase();
+			//comprara cadenas
+			int result=comparaCad(cad,cad2);
+			//mostrar por pantalla 
+			if(result<0) {
+				System.out.println("-1");
+			}
+			if(result>0) {
+				System.out.println("1");
+			}else {
+				System.out.println("0");
+			}
 		
 
+	}
+	public static int comparaCad(String cad, String cad2) {
+		for(int i=0;i<cad.length();i++) {
+			//caracteres de cada cadena
+			char caracad=cad.charAt(i);
+			char caracad2=cad2.charAt(i);
+			if(caracad<caracad2) {
+				return -1;
+			}
+			if(caracad>caracad2) {
+				return 1;
+			}
+		}
+		//si las ccadenas son iguales
+		return 0;
 	}
 
 }
--- programacion/src/ud5/ejercicio339problemas.java
+++ programacion/src/ud5/ejercicio339problemas.java
@@ -1,10 +1,54 @@
 package ud5;
 
+import java.util.Scanner;
+
 public class ejercicio339problemas {
 /* https://codeforces.com/problemset/problem/339/A*/
+	static Scanner entrada=new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("ingrese la suma");
+		String suma=entrada.nextLine();
+		//lamamos funcion
+		String sumaOrganizada=organizaSuma(suma);
+		//muestrab en pantalla la nueva suma
+		System.out.println("la suma reorganizada: "+sumaOrganizada);
 	}
-
+	public static String organizaSuma(String suma) {
+		char[]caracteres=suma.toCharArray();
+		int cont1=0,cont2=0,cont3=0;
+		for(int i=0;i<suma.length();i++) {
+			char cont=suma.charAt(i);
+			if(cont=='1') {
+				cont1++;
+			}
+			if(cont=='2') {
+				cont2++;
+			}
+			if(cont=='3') {
+				cont3++;
+			}
+		}
+		//la nueva suma bn organizada
+		String newsuma="";
+		for(int i=0;i<cont1;i++) {
+			if(!newsuma.isEmpty()) {
+				newsuma+="+";
+			}newsuma+="1";
+				
+		}
+		for(int i=0;i<cont2;i++) {
+			if(!newsuma.isEmpty()) {
+				newsuma+="+";
+			}newsuma+="2";
+				
+		}
+		for(int i=0;i<cont3;i++) {
+			if(!newsuma.isEmpty()) {
+				newsuma+="+";
+			}newsuma+="3";
+				
+		}return newsuma;
+		
+				
+	}
 }
