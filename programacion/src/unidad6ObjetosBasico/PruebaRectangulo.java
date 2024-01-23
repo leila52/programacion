915d0a653a63d1205e56326b6508a2c668de12ae
leila_16 <leila_16@leila>
2024-01-23 16:41:03 +0100

    16:41

--- programacion/src/unidad6ObjetosBasico/PruebaRectangulo.java
+++ programacion/src/unidad6ObjetosBasico/PruebaRectangulo.java
@@ -6,13 +6,23 @@ public class PruebaRectangulo {
 		Rectangulo rectangulo1=new Rectangulo();
 		rectangulo1.area();
 		rectangulo1.perimetro();
-		System.out.println("la altura es : "+ rectangulo1.damealtura()+" y la base es: "+
+		System.out.println("El rectangulo tiene la altura es : "+ rectangulo1.damealtura()+" y la base es: "+
 		rectangulo1.damebase());
 		Rectangulo rectangulo2=new Rectangulo(8,9);
 		rectangulo2.area();
 		rectangulo2.perimetro();
-		System.out.println("la altura es : "+ rectangulo2.damealtura()+" y la base es: "+
+		System.out.println("El rectangulo tiene la altura es : "+ rectangulo2.damealtura()+" y la base es: "+
 		rectangulo2.damebase());
+		Rectangulo rectangulo3=new Rectangulo(15,2);
+		rectangulo3.area();
+		rectangulo3.perimetro();
+		System.out.println("El rectangulo tiene la altura es : "+ rectangulo3.damealtura()+" y la base es: "+
+		rectangulo3.damebase());
+		Rectangulo rectangulo4=new Rectangulo(7,2);
+		rectangulo4.area();
+		rectangulo4.perimetro();
+		System.out.println("El rectangulo tiene la altura es : "+ rectangulo4.damealtura()+" y la base es: "+
+		rectangulo4.damebase());
 		
 		
 
