dbfbf5bef908c0dce518ffebdd84afa71d699730
leila52 <lailahaddad385@gmail.com>
2024-01-29 14:15:04 +0100

    14:15

---
+++ programacion/src/Cuenta2.java
@@ -0,0 +1,23 @@
+
+public class Cuenta2 {
+	private String numeroCuenta;
+	private double Saldo;
+	
+	public Cuenta2(String numeroCuent, double Sald) {
+		numeroCuenta=numeroCuent;
+		Saldo=Sald;
+	}public void IntroducirCuenta(String numeroCuent) {
+		numeroCuenta=numeroCuent;
+	}public void introduceSaldo(double Sald) {
+		Saldo=Sald;
+	}public void abonar (double cantidadAbonar) {
+		Saldo=Saldo+cantidadAbonar;
+	}public boolean pagarRecibo (double cantRecibo) {
+		if (cantRecibo<=Saldo) {
+			Saldo= Saldo-cantRecibo;
+			return true;
+		}else
+			return false;
+	}
+
+}
