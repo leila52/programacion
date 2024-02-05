a588e8e7409f9df3b38a9a94773c404442c4aee1
leila_16 <leila_16@leila>
2024-02-05 20:01:28 +0100

    20:01

--- programacion/src/unidad6ObjetosBasico/Password.java
+++ programacion/src/unidad6ObjetosBasico/Password.java
@@ -16,21 +16,50 @@ public class Password {
 	}
 	//constructor con longitud y genera 
 	//una contraseña aleaotria
-	public static int generarcontrasena(int longi) {
-		int contra = 0;
-		Random aledebil=new Random();
-		int[] longitud=new int [longi];
-		int contrasena;
-		for(int i=0;i<longitud.length;i++) {
-		contrasena=aledebil.nextInt(90)+33;
-		longitud[i]=contrasena;
-		contra=(char)(longitud[i]);
+	public static char generarcontrasena() {
+		Random ale=new Random();
+		// 62 en total 26 mayu 26 min y 10 numeros
+		int caracter=ale.nextInt(62);
+		if(caracter <26) {
+			return (char)(caracter+'A');
 		}
-		return contra ;
+		if(caracter>26 && caracter <52) {
+			return (char)(caracter-26+'a');
+		}
+		else {
+			return (char)(caracter-52+'0');
+		}
 	}
 	//boolean de es fuerte
 	public boolean esFuerte() {
-		
+		int mayu=0;
+		int min=0;
+		int num=0;
+		for(int i=0;i< longitud;i++) {
+			char caracter= generarcontrasena();
+			if(Character.isUpperCase(caracter)) {
+				mayu++;
+			}
+			if(Character.isLowerCase(caracter)) {
+				min++;
+			}
+			if(Character.isDigit(caracter)) {
+				num++;
+			}
+		}
+		return character ;
+	}
+	//getter
+	public String getConttrasena() {
+		return contrasena;
+	}
+	public int getLongitud() {
+		return longitud;
+	}
+	//setters
+	public void setLongitud(int longi) {
+		longitud=longi;
+		generarcontrasena();
 	}
 	
 }
---
+++ programacion/src/unidad6ObjetosBasico/usarPassword.java
@@ -0,0 +1,17 @@
+package unidad6ObjetosBasico;
+
+import java.util.Scanner;
+
+public class usarPassword {
+
+	public static void main(String[] args) {
+		Scanner in =new Scanner(System.in);
+		System.out.println("introduce la longitud");
+		int longContra=in.nextInt();
+		//array
+		Password[] contraseñas= new Password[longContra];
+		boolean [] esFuerteArray =new boolean[longContra];
+
+	}
+
+}
