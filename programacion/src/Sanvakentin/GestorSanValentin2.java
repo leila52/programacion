10b3352b8203175222a0edcf459a8d4debd982f1
leila_16 <leila_16@leila>
2024-02-12 17:08:52 +0100

    20:00

--- programacion/src/Sanvakentin/GestorSanValentin2.java
+++ programacion/src/Sanvakentin/GestorSanValentin2.java
@@ -4,10 +4,11 @@ import java.util.Scanner;
 
 public class GestorSanValentin2 {
 	static Scanner entrada=new Scanner(System.in);
-
+	static Pareja [] parejas = new Pareja[10];
+	static int cont=0;
 
 	public static void main(String[] args) {
-		Pareja [] parejas = new Pareja[10];
+		
 		int op;
 		do {
 			menu();
@@ -35,22 +36,15 @@ public class GestorSanValentin2 {
 	}
 	
 	public static void agregarinfo() {
+		entrada.nextLine();
 		System.out.println("introduca dni persona1 ");
 		String dni1=entrada.nextLine();
+		
 		System.out.println("introduzca su nombre persona1 ");
 		String nom1=entrada.nextLine();
 		Persona perso1 = new Persona(dni1,nom1);
 		
 		
-		System.out.println("el nombre del regalo");
-		String nom1rega=entrada.nextLine();
-		System.out.println("el precio del regalo que va a hacer");
-		double precio1regalo=entrada.nextDouble();
-		System.out.println("la descripccion del regalo ");
-		String desc1regal=entrada.nextLine();
-		Regalo regalo1 = new Regalo(nom1rega,precio1regalo,desc1regal);
-		
-		entrada.nextLine();
 		System.out.println("introduca dni persona2 ");
 		String dni2=entrada.nextLine();
 		System.out.println("introduzca su nombre persona2 ");
@@ -58,16 +52,28 @@ public class GestorSanValentin2 {
 		Persona perso2 = new Persona(dni2,nom2);
 		
 		System.out.println("el nombre del regalo");
+		String nom1rega=entrada.nextLine();
+		System.out.println("el precio del regalo que va a hacer");
+		double precio1regalo=entrada.nextDouble();
+		entrada.nextLine();
+		System.out.println("la descripccion del regalo ");
+		String desc1regal=entrada.nextLine();
+		Regalo regalo1 = new Regalo(nom1rega,precio1regalo,desc1regal);
+		
+		System.out.println("el nombre del regalo");
 		String nom2rega=entrada.nextLine();
 		System.out.println("el precio del regalo que va a hacer");
 		double precio2regalo=entrada.nextDouble();
+		entrada.nextLine();
 		System.out.println("la descripccion del regalo ");
 		String desc2regal=entrada.nextLine();
 		Regalo regalo2 = new Regalo(nom2rega,precio2regalo,desc2regal);
 		
 		Pareja pareja = new Pareja(perso1,perso2);
+		parejas[cont]=pareja;
 		pareja.setRegaloparapersona1(regalo1);
 		pareja.setRegaloparapersona2(regalo2);
+		cont++;
 	}
 	static void mostrarDetallesParejas(Pareja pareja) {
 		pareja.CosteTotal();
