84edaf145a5376d7db160dcef5c26e2c0be49e1a
leila52 <lailahaddad385@gmail.com>
2024-03-06 12:35:00 +0100

    gdfxg

--- programacion/src/ud7POOavanzado/usoPersona.java
+++ programacion/src/ud7POOavanzado/usoPersona.java
@@ -1,10 +1,25 @@
 package ud7POOavanzado;
 
-public class usoPersona {
+import java.util.Scanner;
 
+public class usoPersona { 
+		static Scanner in=new Scanner(System.in);
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Persona [] persona=new Persona[10];
+		String nombre, apellido,dni,estado_civil;
+		for(int i=0; i<persona.length;i++) {
+			System.out.println("que quieres q se añada un estudiante,un profesor o un personal");
+			String respuest=in.nextLine();
+			if(respuest.equals("estudiante")) {
+				System.out.println("introduce el nombre");
+				System.out.println("introduce apellido");
+				System.out.println("introduce dni");
+				System.out.println("introduce estado_civil");
+				System.out.println("introde el curso");
+				
+			}
+		}
 	}
 
 }
