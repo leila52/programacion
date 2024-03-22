2c9b5a21f94b4f0425c2e36753adb477df4f01b0
leila52 <lailahaddad385@gmail.com>
2024-03-22 17:41:05 +0100

    sxsss

--- programacion/src/Veterinariatrabajo3trim/Animal.java
+++ programacion/src/Veterinariatrabajo3trim/Animal.java
@@ -1,5 +1,23 @@
 package Veterinariatrabajo3trim;
 
-public class Animal {
-
+public abstract class Animal {
+	
+	private String nombre,especie;
+	
+	public Animal(String nombre,String especie) {
+		this.nombre=nombre;
+		this.especie=especie;
+	}
+	//metodo abstracto solo se define la cabecera
+	public abstract void DameFichaAnimal();
+	
+	public String getNombre() {
+		return nombre;
+	}
+	public String getEspecie() {
+		return especie;
+	}
+	
+	
 }
+
